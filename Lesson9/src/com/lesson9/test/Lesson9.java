7ba51a28efca6f75c04d8558d329700c7461d442
anhnt-ptit <anhnt.ptit@gmail.com>
2016-10-13 18:50:08 +0700

    commit from server
    commit from server

--- Lesson9/src/com/lesson9/test/Lesson9.java
+++ Lesson9/src/com/lesson9/test/Lesson9.java
@@ -4,7 +4,7 @@ public class Lesson9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Lesson9 commit");
+		System.out.println("Lesson9 commit from server");
 	}
 
 }
