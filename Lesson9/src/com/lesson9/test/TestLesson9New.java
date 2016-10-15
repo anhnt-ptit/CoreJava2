f203661c2508b97293bc028ee508faa5ed4036cb
anh ngo <anhnt.ptit@gmail.com>
2016-10-15 09:52:34 +0700

    this is add new java class for lesson 9

---
+++ Lesson9/src/com/lesson9/test/TestLesson9New.java
@@ -0,0 +1,11 @@
+package com.lesson9.test;
+
+public class TestLesson9New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test for lesson 9");
+
+	}
+
+}
